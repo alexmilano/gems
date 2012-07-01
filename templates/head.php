24729a0a44628d253a29530f4c0b0ea20ee6982f
Ignacio Cordoba <icordoba@stcsolutions.com.ve>
2012-06-30 20:30:32 -0430

    scripts del demonio

--- templates/head.php
+++ templates/head.php
@@ -7,7 +7,7 @@ start rendering de content view.
 
   <title>GEMS</title>
 
-  <?php if($_REQUEST['view']==$GLOBALS["LOGIN_VIEW"] || $_REQUEST['view']==$GLOBALS["SIGNUP_VIEW"] || $_REQUEST['view']==$GLOBALS["CHANGE_PASSWORD_VIEW"]){ ?>
+  <?php //if($_REQUEST['view']==$GLOBALS["LOGIN_VIEW"] || $_REQUEST['view']==$GLOBALS["SIGNUP_VIEW"] || $_REQUEST['view']==$GLOBALS["CHANGE_PASSWORD_VIEW"]){ ?>
   	<script type="text/javascript" src="js/sha1.js"></script>
-  <?php } ?>
+  <?php //} ?>
 <?php } ?>

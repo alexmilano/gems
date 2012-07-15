ae00f71e48c6753ecd29e8c18a33232d400444b6
Alexander Milano <alex.milano@gmail.com>
2012-07-15 18:33:03 -0430

    panel de supervisor

--- view/generated/panel-supervisor.php
+++ view/generated/panel-supervisor.php
@@ -1,4 +1,7 @@
 <a href="list-profile&status=Valido" class="btn">Ver Socios</a>
 <a href="list-profile&status=Pendiente" class="btn">Ver Aspirantes</a>
 <a href="list-profile&status=Inactivo" class="btn">Ver Inactivos</a>
+<a href="list-venta" class="btn">Ventas</a>
+<a href="list-premio" class="btn">Premios</a>
+<a href="list-canje&status=Pendiente" class="btn">Canjes</a>
 <a href="crud.php?close_session" class="btn">Cerrar Sesi&oacute;n</a>
